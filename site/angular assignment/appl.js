e9c018e0221ad41c22d0626636f90200c3f59b30
kavyathara <40180098+kavyathara@users.noreply.github.com>
2019-02-06 23:22:48 -0500

    Update appl.js

--- site/angular assignment/appl.js
+++ site/angular assignment/appl.js
@@ -1,41 +1,29 @@
+(function(){
+	'use strict';
+	angular.module('LunchCheck',[])
+	.controller('LunchCheckController',LunchCheckController);
+	LunchCheckController.$inject=['$scope']
+	function LunchCheckController($scope){
+		$scope.enterMenu='';
+		$scope.showMessage='';
+		$scope.showMenu=function (){
+			var arr=[];
+			var menuItem=$scope.enterMenu;
+			if(menuItem.length==0){
+				$scope.showMessage="Please enter data first";
+			}else{
+				 arr=menuItem.split(",");
+			 var itemLength=arr.length;
+			 if(itemLength<4){
+			$scope.showMessage="Enjoy!";
+			 }else{
+				 $scope.showMessage="Too much!";
+			 }
 
+			}
 
-(function () {
-'use strict';
+		}
+	};
 
-angular.module('LunchCheck', [])
-.controller('LunchCheckController', LunchCheckController);
-
-LunchCheckController.$inject = ['$scope'];
-function LunchCheckController($scope) {
-  $scope.checkDishes = function () {
-    var num = countDishes($scope.dishes);
-    $scope.message = buildMessage(num);
-  };
-
-  function countDishes(dishes) {
-    var count = 0;
-    if (dishes) {
-      var array = dishes.split(',');
-      for (var idx in array) {
-        if (array[idx].trim().length != 0) {
-          count++;
-        }
-      }
-    }
-    return count;
-  }
-
-  function buildMessage(num) {
-    if (num === 0) {
-      return 'Please enter data first';
-    }
-    else if (num <= 3) {
-      return 'Enjoy!';
-    } else {
-      return 'Too much!';
-    }
-  }
-}
 
 })();
